ddb9ad4e8b5b725b6b12e6f741df6625749c0e06
unknown <mysuccess@daum.net>
2018-08-22 05:43:27 +0900

    Add GET /users/me and authentication middleware

--- server/server.js
+++ server/server.js
@@ -16,6 +16,9 @@ var {
 var {
     User
 } = require("./models/user");
+var {
+    authenticate
+} = require('./middleware/authenticate');
 
 var app = express();
 const port = process.env.PORT || 3000;
@@ -73,6 +76,13 @@ app.post("/users", (req, res) => {
             res.status(400).send(e);
         });
 });
+
+
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.send(req.user);
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
