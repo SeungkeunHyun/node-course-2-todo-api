06813df897c5e51ad512cf86367bef2bba9d8481
unknown <mysuccess@daum.net>
2018-08-23 07:29:32 +0900

    Add DELETE /users/me/token route

--- server/server.js
+++ server/server.js
@@ -133,6 +133,14 @@ app.get('/users/me', authenticate, (req, res) => {
     res.send(req.user);
 });
 
+app.delete('/users/me/token', authenticate, (req, res) => {
+    req.user.removeToken(req.token).then(() => {
+        res.status(200).send();
+    }, (err) => {
+        res.status(400).send();
+    });
+});
+
 app.post('/users/login', (req, res) => {
     var body = _.pick(req.body, ['email', 'password']);
     console.log(body);
@@ -166,6 +174,7 @@ app.post('/users', (req, res) => {
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
+
 
 module.exports = {
     app
